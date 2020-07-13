ad9d2414ba72393bebe185b45c7d7463719201d0
Shasika Udayanga <45961723+shasikaud@users.noreply.github.com>
2020-07-13 23:52:49 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
+//create element and render cafe
+function renderCafe(doc){
+
+     let li = document.createElement('li');
+     let name = document.createElement('span');
+     let city = document.createElement('span');
+     let cross = document.createElement('div');
+
+     li.setAttribute('data-id', doc.id);
+     name.textContent = doc.data().name;
+     city.textContent = doc.data().city;
+     cross.textContent = 'x';  
+ 
+     li.appendChild(name);
+     li.appendChild(city);
+     li.appendChild(cross);
+
+     cafeList.appendChild(li);
+
+     //deleting data
+     cross.addEventListener('click', (e) => {
+         e.stopPropagation();
+         let id = e.target.parentElement.getAttribute('data-id');
+         db.collection('cafes').doc(id).delete();
+     })
+}
+
+
+//get date from firestore
+//db.collection('cafes').get().then((snapshot) => {
+//    //console.log(snapshot.docs);
+//   snapshot.docs.forEach(doc => {
+//       renderCafe(doc);
+//    });
+//});
+
+
+
+//listen for a submit event on the form
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+});
+
+//real-time listener to firestore database
+db.collection('cafes').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if (change.type == 'added'){
+            renderCafe(change.doc);
+        } else if (change.type == 'removed'){
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    })
+})
